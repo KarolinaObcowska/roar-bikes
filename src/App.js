ce44ba79a9399d43f2cbced7fd346b2ad8b5996d
Karolina Obcowska <jimmy.obcowska@gmail.com>
2022-01-02 23:42:34 +0100

    fixed footer

--- src/App.js
+++ src/App.js
@@ -3,9 +3,10 @@ import Navbar from "./components/Navbar/Navbar";
 import LandingPage from "./pages/LandingPage/LandingPage";
 import Cart from "./pages/Cart/Cart";
 import Footer from "./components/Footer/Footer";
+import "./sassStyles/_global.sass";
 function App() {
   return (
-    <div className="App">
+    <div className="app">
       <Navbar />
       <Cart />
       {/* <LandingPage /> */}
