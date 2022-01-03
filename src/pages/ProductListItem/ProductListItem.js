616b68ebb745cc84c93761a2062f532d7326c714
Karolina Obcowska <jimmy.obcowska@gmail.com>
2022-01-03 18:26:25 +0100

    item description added

--- src/pages/ProductListItem/ProductListItem.js
+++ src/pages/ProductListItem/ProductListItem.js
@@ -52,12 +52,27 @@ const ProductListItem = () => {
       <div className="item__details-container">
         {/* <button className="details__btn">DESCRIPTION</button>
         <button className="details__btn">SPECIFICATION</button> */}
+
         <div className="item__details">
           <div className="details__text">
-            <h3 className="details__title">asd</h3>
-            <p className="details__description">asd</p>
-            <h3 className="details__title">asd</h3>
-            <p className="details__description">asd</p>
+            <h3 className="details__title">IT IS RIGHT FOR YOU IF...</h3>
+            <p className="details__description">
+              The Siamese is long and slack up front, short and snappy out back. We kept the center
+              of gravity low and gave the Shuttle an event burlier version of the rear shock linkage
+              featured on out long-travel bikes. Finally, we maximized frame and wheel stiffness
+              with Suber Boost Plus 157mm spacing. The end result? The Shuttle manages to be
+              suprosingly nimble in tight conditions, is supemely confident on fast, chunku
+              descents, and can scale ridiculously steep climbs with surefooted calm.
+            </p>
+            <h3 className="details__title">THE TECH YOU GET</h3>
+            <p className="details__description">
+              Siamese specific electric assist 150 watt drive unit system silently doubles the
+              averege riders output, so you ride harder, longer and on trials that prevoiusly seemed
+              un-rideable. The Shuttle is so awesome you can even ride with turned off and guess
+              what? It still shreds. Hard. Just like our class-leading trial and enduro models. That
+              is because it is eassentially a rowdier version of out singletrack loving Mach 5.5
+              trial bike wit whole lot of Firebird DBA mixed in.
+            </p>
           </div>
           <div className="details__photo">
             <img className="details__photo-item" src={Photo1} />
